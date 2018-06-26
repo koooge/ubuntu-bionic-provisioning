7a5f867b4f351d492e5098a3dcbc4bcd9b327b10
koooge <koooooge@gmail.com>
2018-06-26 23:19:52 +0900

    go: Delete duplicate vim-go

--- install_golang.sh
+++ install_golang.sh
@@ -40,14 +40,8 @@ go_get_packages() {
   cp /tmp/peco_linux_amd64/peco ${GOPATH}/bin/
 }
 
-install_vim_go() {
-  curl -sSfLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  git clone https://github.com/fatih/vim-go.git ~/.vim/plugged/vim-go
-}
-
 # install_gvm
 install_golang
 go_get_packages
-install_vim_go
 
 go version
