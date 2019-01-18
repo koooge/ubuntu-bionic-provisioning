f35a7c03ccb01a71b33774c2e1c70fe375df7a5d
Takuma Koge <ta-koge@global-mobility-service.com>
2019-01-18 17:57:14 +0900

    kubectl: Move bash_profile into .bash.d/
    Signed-off-by: Takuma Koge <ta-koge@global-mobility-service.com>

--- tool/install_kubectl.sh
+++ tool/install_kubectl.sh
@@ -5,7 +5,3 @@ curl -sS https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key ad
 sudo bash -c 'echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list'
 sudo apt-get update -qq
 sudo apt-get install -y kubectl
-
-echo "alias k='kubectl'" >> ~/.bash_profile
-echo "source <(kubectl completion bash)" >> ~/.bash_profile
-echo "complete -o default -F __start_kubectl k" >> ~/.bash_profile
