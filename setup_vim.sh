189f6422f054c2e10f1bddb892e360ef0c97b939
Takuma Koge <ta-koge@global-mobility-service.com>
2019-01-18 18:30:19 +0900

    vim: Install vim-vue
    Signed-off-by: Takuma Koge <ta-koge@global-mobility-service.com>

--- setup_vim.sh
+++ setup_vim.sh
@@ -3,3 +3,6 @@ mkdir -p ~/.vim/pack/tpope/start
 cd ~/.vim/pack/tpope/start
 git clone https://tpope.io/vim/commentary.git
 vim -u NONE -c "helptags commentary/doc" -c q
+
+# vim-vue
+git clone https://github.com/posva/vim-vue.git ~/.vim/pack/plugins/start/vim-vue
